87ad8c9d764481f7fd3b848fd8fb5c27d406acf4
Lasse Kristensen <lasse.kristensen@assemblyvoting.com>
2023-05-25 14:36:04 +0200

    update

--- src/scripts/tag.sh
+++ src/scripts/tag.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-SEMVER_INCREMENT="prerelease ${CURRENT_BRANCH}."
 
 LAST_MAIN_TAG=$( gh release view --json tagName --jq '.tagName')
 LAST_TAG=$(git tag -l --sort=-version:refname "${LAST_MAIN_TAG}"-"${CURRENT_BRANCH}"* | head -n 1)
@@ -8,14 +7,12 @@ LAST_TAG=$(git tag -l --sort=-version:refname "${LAST_MAIN_TAG}"-"${CURRENT_BRAN
 echo "Lastest release $LAST_TAG"
 if [ -z "$LAST_TAG" ]; then
         echo "could not find the last tag"
-        echo "semver bump ${SEMVER_INCREMENT} ${LAST_MAIN_TAG}-${CURRENT_BRANCH}"
-        NEW_TAG=$(semver bump "${SEMVER_INCREMENT}" "${LAST_MAIN_TAG}"-"${CURRENT_BRANCH}")
+        NEW_TAG=$(semver bump prerelease "$CURRENT_BRANCH" "$LAST_MAIN_TAG"-"$CURRENT_BRANCH")
 else
-        NEW_TAG=$(semver bump "${SEMVER_INCREMENT}" "${LAST_TAG}")
+        NEW_TAG=$(semver bump prerelease "$CURRENT_BRANCH" "${LAST_TAG}")
 fi
 
 FINAL_TAG="v$NEW_TAG"
-git tag "$FINAL_TAG"
-git push origin "$FINAL_TAG"
+#git tag "$FINAL_TAG"
+#git push origin "$FINAL_TAG"
 echo "Tag $FINAL_TAG created."
-
